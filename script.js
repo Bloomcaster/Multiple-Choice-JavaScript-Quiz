5fae98e1e58f430026d33dc104ebd9eba5befe95
Tahlia <tbolden13@gmail.com>
2022-09-09 22:17:16 -0700

    need to fix start button issues, no click

---
+++ script.js
@@ -0,0 +1,23 @@
+const startButton = document.getElementbyId('start-btn')
+startButton.addEventListener('click', startGame)
+
+
+
+
+
+
+
+function startGame(){
+  
+
+}
+
+
+function nextQuestion() {
+
+}
+
+
+function chooseAnswer(){
+
+}
